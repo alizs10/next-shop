e5dd42204d455cc6daae21e51eb5aee51d78a6a6
alizs10 <ali.text77@gmail.com>
2022-11-22 17:59:21 +0330

    update: add links

--- components/Head.js
+++ components/Head.js
@@ -9,7 +9,9 @@ function Head() {
       <div className='relative px-5 py-2 grid grid-cols-12 bg-orange-200'>
         <div className='col-span-3 flex justify-center items-center font-bold text-2xl'>
           <img className='w-20' src='/assets/icons/nike.svg' />
-          <span className='ml-5'>Nike's Shoes</span>
+          <Link href="/">
+            <span className='ml-5'>Nike's Shoes</span>
+          </Link>
         </div>
         <ul className='col-span-7 flex justify-center items-center gap-8 text-base'>
           <li className='underline underline-offset-4 cursor-pointer'>
@@ -37,7 +39,11 @@ function Head() {
             <PopoverContent>
               <PopoverArrow />
               <PopoverCloseButton />
-              <PopoverHeader>Cart</PopoverHeader>
+              <PopoverHeader>
+                <Link href="/cart">
+                  Cart
+                </Link>
+              </PopoverHeader>
               <PopoverBody>
                 <div className='flex flex-col'>
                   <li className='grid grid-cols-4 gap-2 items-center'>
