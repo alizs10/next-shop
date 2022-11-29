380e6418798360e4d5499de8bdce2d265398ee8e
alizs10 <ali.text77@gmail.com>
2022-11-29 17:38:31 +0330

    update: add close button to filter popover

--- components/Filter.js
+++ components/Filter.js
@@ -1,3 +1,4 @@
+import { CloseIcon } from '@chakra-ui/icons'
 import { Button } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import ColorFilter from './Filters/ColorFilter'
@@ -10,6 +11,7 @@ function Filter() {
 
     const [isOpen, setIsOpen] = useState(false)
     const openFilterPopover = () => {
+        if(isOpen) return
         setIsOpen(true)
     }
     const closeFilterPopover = () => {
@@ -57,21 +59,31 @@ function Filter() {
         <>
             <div
                 onClick={openFilterPopover}
-                className={`absolute flex ${isOpen ? 'top-1/4 left-1/3 w-1/3 h-fit rounded-3xl flex-col gap-y-2 px-5 py-3 bg-gray-100 z-[60]' : 'z-40 hover:translate-x-2 top-1/2 justify-center items-center left-0 w-[6vmin] h-[17vmin] rounded-r-3xl bg-orange-200'}
-        shadow-md cursor-pointer transition-all duration-300`}>
-                <span className={`${isOpen ? 'text-xl mb-2' : '-rotate-90 text-md'} flex gap-x-2`}>
-                    <span>
-                        <FilterIcon />
+                className={`absolute flex ${isOpen ? 'top-1/4 left-1/3 w-1/3 h-fit rounded-3xl flex-col gap-y-2 px-5 py-3 bg-gray-100 z-[60]' : 'cursor-pointer z-40 hover:translate-x-2 top-1/2 justify-center items-center left-0 w-[6vmin] h-[17vmin] rounded-r-3xl bg-orange-200'}
+        shadow-md transition-all duration-300`}>
+                <span className={`${isOpen ? 'text-xl mb-2 justify-between items-center' : '-rotate-90 text-md'} flex gap-x-2`}>
+                    <span className='flex gap-x-2'>
+                        <span>
+                            <FilterIcon />
+                        </span>
+                        <span>
+                            Filter
+                        </span>
                     </span>
-                    <span>
-                        Filter
-                    </span>
+                    {isOpen && (
+
+                        <button
+                            onClick={closeFilterPopover}
+                            className='rounded-full hover:bg-slate-200 transition-all duration-300 p-3 flex justify-center items-center'>
+                            <CloseIcon fontSize='small' />
+                        </button>
+                    )}
                 </span>
                 {isOpen && (
                     <div className='flex flex-col gap-2'>
                         <PriceRange defaultValue={priceRangeValue} handleChange={handlePriceRangeChange} />
                         <SizeFilter sizes={sizes} onChange={updateSizes} />
-                        <ColorFilter colors={colors} onChange={updateColors}/>
+                        <ColorFilter colors={colors} onChange={updateColors} />
                         <Button marginTop='8' colorScheme='orange'>Filter</Button>
                     </div>
                 )}
