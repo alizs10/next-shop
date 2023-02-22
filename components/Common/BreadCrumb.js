db1c928f673defbadd352d396f26c4b0f3082ef6
alizs10 <ali.text77@gmail.com>
2023-02-22 15:44:34 +0330

    update: breadcrumb UI

--- components/Common/BreadCrumb.js
+++ components/Common/BreadCrumb.js
@@ -17,7 +17,7 @@ function BCLink({ path, isLast, isFirst }) {
     return (
         <Link href={path.url}>
             <div onMouseEnter={handleMouseIn} onMouseLeave={handleMouseOut} className={`relative h-fit bg-gray-100 cursor-pointer transition-all duration-300 ${path.current ? 'bg-gray-200' : 'hover:bg-gray-200'}`}>
-                <div className={`${isFirst ? 'pr-10' : 'pr-10 pl-14'} px-5 py-3`}>{path.name}</div>
+                <div className={`relative z-50 ${!isFirst && 'pl-12'} px-3 py-3`}>{path.name}</div>
                 {!isLast && (
                     <div className={`absolute top-0 right-0 border-r-2 border-t-2 border-gray-300 translate-x-1/2 rotate-45 h-full aspect-square bg-gray-100 z-20 cursor-pointer transition-all duration-300 ${isHover && 'bg-gray-200'}`}></div>
                 )}
