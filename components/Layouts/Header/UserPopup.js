ca27c8addd1a9a3b006e4aa3c25ac28ad6d133cb
alizs10 <ali.text77@gmail.com>
2023-03-06 23:33:58 +0330

    update: improved user popup

--- components/Layouts/Header/UserPopup.js
+++ components/Layouts/Header/UserPopup.js
@@ -33,30 +33,42 @@ function UserPopup({ user }) {
             {showPopup && (
                 <>
                     <div className='absolute z-[999999] top-20 right-0 min-w-[250px] overflow-hidden flex flex-col rounded-xl border-2 border-gray-200 bg-gray-50'>
-                        <div className='flex justify-between p-3 items-center border-b-2 bg-gray-100 border-gray-200 cursor-pointer'>
+                        <div className='flex justify-between p-3 items-center border-b-2 bg-gray-50 border-gray-200 cursor-pointer'>
                             <div className='flex items-center gap-x-2'>
                                 <Image className='w-10 rounded-full' src="/assets/avatars/avatar.jpeg" width={200} height={200} />
-                                <span>{user.fullName}</span>
+                                <div className='flex flex-col'>
+                                    <span className='text-lg'>{user.fullName}</span>
+                                    <span className='text-xs text-gray-500'>{user.email}</span>
+
+                                </div>
                             </div>
                             <span className='text-gray-500'>
                                 <ChevronRightIcon />
                             </span>
                         </div>
                         <div className='flex gap-x-2 p-3 items-center text-gray-600 border-b-2 border-gray-100 transition-all duration-300 hover:bg-gray-100 cursor-pointer'>
-                            <HeartIcon />
-                            <span>Favorites</span>
+                            <span className='scale-[80%]'>
+                                <HeartIcon />
+                            </span>
+                            <span className='text-sm'>Favorites</span>
                         </div>
                         <div className='flex gap-x-2 p-3 items-center text-gray-600 border-b-2 border-gray-100 transition-all duration-300 hover:bg-gray-100 cursor-pointer'>
-                            <ListIcon />
-                            <span>Orders</span>
+                            <span className='scale-[80%]'>
+                                <ListIcon />
+                            </span>
+                            <span className='text-sm'>Orders</span>
                         </div>
                         <div className='flex gap-x-2 p-3 items-center text-gray-600 border-b-2 border-gray-100 transition-all duration-300 hover:bg-gray-100 cursor-pointer'>
-                            <MapIcon />
-                            <span>Addresses</span>
+                            <span className='scale-[80%]'>
+                                <MapIcon />
+                            </span>
+                            <span className='text-sm'>Addresses</span>
                         </div>
                         <div onClick={handleSignOut} className='flex gap-x-2 p-3 items-center text-gray-600 border-b-2 border-gray-100 transition-all duration-300 hover:bg-gray-100 cursor-pointer'>
-                            <LogoutIcon />
-                            <span>Logout</span>
+                            <span className='scale-[80%]'>
+                                <LogoutIcon />
+                            </span>
+                            <span className='text-sm'>Logout</span>
                         </div>
                     </div>
                     <Backdrop toggler={showPopup} handleClick={togglePopup} />
