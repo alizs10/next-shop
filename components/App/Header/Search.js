7494fb719d7b53eb5fa1595f1c6691300e11d1c1
alizs10 <ali.text77@gmail.com>
2023-03-15 00:03:29 +0330

    update: add animations to search

--- components/App/Header/Search.js
+++ components/App/Header/Search.js
@@ -1,6 +1,9 @@
+import { AnimatePresence } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import SearchIcon from "../../ui/icons/SearchIcon";
 import XIcon from "../../ui/icons/XIcon";
+
+import { motion } from 'framer-motion';
 
 function Search() {
 
@@ -23,17 +26,52 @@ function Search() {
     return (
         <div className="relative mr-3">
             <div onClick={toggleSearch} className="lg:scale-125 w-10 cursor-pointer text-red-500">
-                {isExpanded ? <XIcon /> : <SearchIcon />}
+                <AnimatePresence mode="wait" initial={false}>
+                    {isExpanded && (
+                        <motion.div
+                            key={0}
+                            initial={{ y: 0, opacity: [0, 1] }}
+                            animate={{ y: 0, opacity: 1 }}
+                            exit={{ y: [0, 20], opacity: 0 }}
+                            transition={{ bounce: "spring", duration: ".3" }}
+                        >
+                            <XIcon />
+                        </motion.div>
+                    )}
+
+                    {!isExpanded && (
+                        <motion.div
+                            key={1}
+                            initial={{ y: 0, opacity: [0, 1] }}
+                            animate={{ y: 0, opacity: 1 }}
+                            exit={{ y: [0, 20], opacity: 0 }}
+                            transition={{ bounce: "spring", duration: ".3" }}
+                        >
+                            <SearchIcon />
+
+                        </motion.div>
+                    )}
+
+                </AnimatePresence>
+
             </div>
 
-            {isExpanded && (
-                <div className="absolute bottom-0 right-full mr-4 flex gap-x-2 items-end">
-                    <span className="text-red-500 scale-125 cursor-pointer">
-                        <SearchIcon />
-                    </span>
-                    <input ref={searchInputRef} className="py-2 px-3 border-2 border-red-500 bg-gray-700 rounded-xl text-md text-red-400 outline-none" />
-                </div>
-            )}
+            <AnimatePresence>
+                {isExpanded && (
+                    <motion.div
+                        key={2}
+                        initial={{ y: 0, opacity: 1 }}
+                        animate={{ x: [100, 0], opacity: [0, 1] }}
+                        exit={{ y: [0, 20], opacity: 0 }}
+                        transition={{ bounce: "none", duration: ".3" }}
+                        className="absolute bottom-0 right-full mr-4 flex gap-x-2 items-end">
+                        <span className="text-red-500 scale-125 cursor-pointer">
+                            <SearchIcon />
+                        </span>
+                        <input ref={searchInputRef} className="py-2 px-3 border-2 border-red-500 bg-gray-700 rounded-xl text-md text-red-400 outline-none" />
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 }
