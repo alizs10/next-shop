fb8bc8e9f79b42132fe72976b2d3f891219b0def
alizs10 <ali.text77@gmail.com>
2023-03-19 01:48:06 +0330

    some fixes

--- util/database-util.js
+++ util/database-util.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 export async function connectDatabase(database) {
-    await mongoose.connect(`mongodb://localhost:27017/${database}`)
+    await mongoose.connect(`mongodb://127.0.0.1:27017/${database}`)
 }
 
 export function closeConnection() {
