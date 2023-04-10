3f1740a2d02a232ac4c23e0c26f8537d325fedc5
alizs10 <ali.text77@gmail.com>
2023-04-10 15:47:07 +0330

    update: add moving border to profile layout

--- components/App/Profile/ProfileLayout.js
+++ components/App/Profile/ProfileLayout.js
@@ -1,43 +1,97 @@
+import React, { useState, useRef, useEffect } from 'react';
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 function ProfileLayout({ children, data }) {
 
+    useEffect(() => {
+        placeBorder()
+
+    }, [])
+
+    function placeBorder() {
+        let index = 0;
+
+        if (pathname === '/profile/favorites') {
+            index = 1;
+        }
+        if (pathname === '/profile/orders') {
+            index = 2;
+        }
+        if (pathname === '/profile/addresses') {
+            index = 3;
+        }
+        if (pathname === '/profile/payments') {
+            index = 4;
+        }
+
+        handleTabHover(index)
+    }
+
     const { pathname } = useRouter()
+
+    useEffect(() => {
+
+        setIsRedirecting(false)
+
+    }, [pathname])
+
+
+    const [isRedirecting, setIsRedirecting] = useState(false)
+
+    const navRef = useRef();
+    const borderRef = useRef();
+
+    function handleTabHover(index) {
+        if (isRedirecting) return
+
+        const nav = navRef.current;
+        const border = borderRef.current;
+        const tab = nav.children[index];
+        border.style.width = `${tab.offsetWidth}px`;
+        border.style.left = `${tab.offsetLeft}px`;
+    }
+
+
 
     return (
         <section className="p-20">
-            <ul className="flex flex-nowrap gap-x-4 w-full overflow-x-scroll no-scrollbar">
-                <Link href='/profile'>
-                    <li className={`whitespace-nowrap min-w-fit text-center text-2xl text-white py-3 px-5 border-b-[3px] transition-all duration-300 ${pathname === '/profile' ? 'border-red-500' : 'border-gray-800'}`}>
-                        Profile Information
-                    </li>
-                </Link>
-                <Link href='/profile/favorites'>
-                    <li className={`whitespace-nowrap min-w-fit flex items-center gap-x-2 text-center text-2xl text-white py-3 px-5 border-b-[3px] transition-all duration-300 ${pathname === '/profile/favorites' ? 'border-red-500' : 'border-gray-800'}`}>
-                        <span>Favorites</span>
-                        <div className="rounded-md w-7 aspect-square bg-gray-700 text-gray-200 text-sm flex justify-center items-center">{data.favoritesCount}</div>
-                    </li>
-                </Link>
-                <Link href='/profile/orders'>
-                    <li className={`whitespace-nowrap min-w-fit flex items-center gap-x-2 text-center text-2xl text-white py-3 px-5 border-b-[3px] transition-all duration-300 ${pathname === '/profile/orders' ? 'border-red-500' : 'border-gray-800'}`}>
-                        <span>Orders</span>
-                        <div className="rounded-md w-7 aspect-square bg-gray-700 text-gray-200 text-sm flex justify-center items-center">{data.ordersCount}</div>
-                    </li>
-                </Link>
-                <Link href='/profile/addresses'>
-                    <li className={`whitespace-nowrap min-w-fit flex items-center gap-x-2 text-center text-2xl text-white py-3 px-5 border-b-[3px] transition-all duration-300 ${pathname === '/profile/addresses' ? 'border-red-500' : 'border-gray-800'}`}>
-                        <span>Addresses</span>
-                        <div className="rounded-md w-7 aspect-square bg-gray-700 text-gray-200 text-sm flex justify-center items-center">{data.addressesCount}</div>
-                    </li>
-                </Link>
-                <Link href='/profile/payments'>
-                    <li className={`whitespace-nowrap min-w-fit flex items-center gap-x-2 text-center text-2xl text-white py-3 px-5 border-b-[3px] transition-all duration-300 ${pathname === '/profile/payments' ? 'border-red-500' : 'border-gray-800'}`}>
-                        <span>Payments</span>
-                        <div className="rounded-md w-7 aspect-square bg-gray-700 text-gray-200 text-sm flex justify-center items-center">{data.paymentsCount}</div>
-                    </li>
-                </Link>
-            </ul>
+
+            <div className='pb-1 relative'>
+
+                <ul ref={navRef} className="flex flex-nowrap gap-x-4 w-full overflow-x-scroll no-scrollbar">
+                    <Link onClick={() => setIsRedirecting(true)} href='/profile'>
+                        <li onMouseEnter={() => handleTabHover(0)} onMouseLeave={placeBorder} className={`whitespace-nowrap min-w-fit text-center text-2xl text-white py-3 transition-all duration-300`}>
+                            Profile Information
+                        </li>
+                    </Link>
+                    <Link onClick={() => setIsRedirecting(true)} href='/profile/favorites'>
+                        <li onMouseEnter={() => handleTabHover(1)} onMouseLeave={placeBorder} className={`whitespace-nowrap min-w-fit flex items-center gap-x-2 text-center text-2xl text-white py-3 transition-all duration-300`}>
+                            <span>Favorites</span>
+                            <div className="rounded-md w-7 aspect-square bg-gray-700 text-gray-200 text-sm flex justify-center items-center">{data.favoritesCount}</div>
+                        </li>
+                    </Link>
+                    <Link onClick={() => setIsRedirecting(true)} href='/profile/orders'>
+                        <li onMouseEnter={() => handleTabHover(2)} onMouseLeave={placeBorder} className={`whitespace-nowrap min-w-fit flex items-center gap-x-2 text-center text-2xl text-white py-3 transition-all duration-300`}>
+                            <span>Orders</span>
+                            <div className="rounded-md w-7 aspect-square bg-gray-700 text-gray-200 text-sm flex justify-center items-center">{data.ordersCount}</div>
+                        </li>
+                    </Link>
+                    <Link onClick={() => setIsRedirecting(true)} href='/profile/addresses'>
+                        <li onMouseEnter={() => handleTabHover(3)} onMouseLeave={placeBorder} className={`whitespace-nowrap min-w-fit flex items-center gap-x-2 text-center text-2xl text-white py-3 transition-all duration-300 `}>
+                            <span>Addresses</span>
+                            <div className="rounded-md w-7 aspect-square bg-gray-700 text-gray-200 text-sm flex justify-center items-center">{data.addressesCount}</div>
+                        </li>
+                    </Link>
+                    <Link onClick={() => setIsRedirecting(true)} href='/profile/payments'>
+                        <li onMouseEnter={() => handleTabHover(4)} onMouseLeave={placeBorder} className={`whitespace-nowrap min-w-fit flex items-center gap-x-2 text-center text-2xl text-white py-3 transition-all duration-300 `}>
+                            <span>Payments</span>
+                            <div className="rounded-md w-7 aspect-square bg-gray-700 text-gray-200 text-sm flex justify-center items-center">{data.paymentsCount}</div>
+                        </li>
+                    </Link>
+                </ul>
+                <div ref={borderRef} className='h-1 bottom-0 transition-all duration-300 bg-red-500 absolute'></div>
+            </div>
 
             {children}
         </section>
