4d9dda0bd01c28fd311038a8aa1e4c6fccd4a249
alizs10 <ali.text77@gmail.com>
2023-04-10 17:11:58 +0330

    update: improved profile layout

--- components/App/Profile/ProfileLayout.js
+++ components/App/Profile/ProfileLayout.js
@@ -4,13 +4,70 @@ import { useRouter } from "next/router";
 
 function ProfileLayout({ children, data }) {
 
+    const navContainerRef = useRef();
+    const navRef = useRef();
+    const borderRef = useRef();
+
+    const profileRef = useRef()
+    const favoritesRef = useRef()
+    const ordersRef = useRef()
+    const paymentsRef = useRef()
+    const addressesRef = useRef()
+
     useEffect(() => {
         placeBorder()
+
+        scrollToItem(getCurrentIndex())
 
     }, [])
 
     function placeBorder() {
-        let index = 0;
+        handleTabHover(getCurrentIndex())
+    }
+
+    const scrollToItem = (index) => {
+
+        if (pathname === '/profile') {
+            profileRef.current.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+        }
+
+        if (pathname === '/profile/favorites') {
+            favoritesRef.current.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+        }
+        if (pathname === '/profile/orders') {
+            ordersRef.current.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+        }
+        if (pathname === '/profile/addresses') {
+            addressesRef.current.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+        }
+        if (pathname === '/profile/payments') {
+            paymentsRef.current.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+
+        }
+
+    };
+
+    function getCurrentIndex() {
+        let index;
+
+        if (pathname === '/profile') {
+            index = 0;
+        }
 
         if (pathname === '/profile/favorites') {
             index = 1;
@@ -23,9 +80,10 @@ function ProfileLayout({ children, data }) {
         }
         if (pathname === '/profile/payments') {
             index = 4;
+
         }
 
-        handleTabHover(index)
+        return index
     }
 
     const { pathname } = useRouter()
@@ -39,8 +97,6 @@ function ProfileLayout({ children, data }) {
 
     const [isRedirecting, setIsRedirecting] = useState(false)
 
-    const navRef = useRef();
-    const borderRef = useRef();
 
     function handleTabHover(index) {
         if (isRedirecting) return
@@ -57,33 +113,33 @@ function ProfileLayout({ children, data }) {
     return (
         <section className="p-20">
 
-            <div className='pb-1 relative'>
+            <div ref={navContainerRef} className='pb-1 relative  overflow-x-scroll no-scrollbar'>
 
-                <ul ref={navRef} className="flex flex-nowrap gap-x-4 w-full overflow-x-scroll no-scrollbar">
-                    <Link onClick={() => setIsRedirecting(true)} href='/profile'>
+                <ul ref={navRef} className="flex flex-nowrap gap-x-4 w-fit">
+                    <Link ref={profileRef} onClick={() => setIsRedirecting(true)} href='/profile'>
                         <li onMouseEnter={() => handleTabHover(0)} onMouseLeave={placeBorder} className={`whitespace-nowrap min-w-fit text-center text-2xl text-white py-3 transition-all duration-300`}>
                             Profile Information
                         </li>
                     </Link>
-                    <Link onClick={() => setIsRedirecting(true)} href='/profile/favorites'>
+                    <Link ref={favoritesRef} onClick={() => setIsRedirecting(true)} href='/profile/favorites'>
                         <li onMouseEnter={() => handleTabHover(1)} onMouseLeave={placeBorder} className={`whitespace-nowrap min-w-fit flex items-center gap-x-2 text-center text-2xl text-white py-3 transition-all duration-300`}>
                             <span>Favorites</span>
                             <div className="rounded-md w-7 aspect-square bg-gray-700 text-gray-200 text-sm flex justify-center items-center">{data.favoritesCount}</div>
                         </li>
                     </Link>
-                    <Link onClick={() => setIsRedirecting(true)} href='/profile/orders'>
+                    <Link ref={ordersRef} onClick={() => setIsRedirecting(true)} href='/profile/orders'>
                         <li onMouseEnter={() => handleTabHover(2)} onMouseLeave={placeBorder} className={`whitespace-nowrap min-w-fit flex items-center gap-x-2 text-center text-2xl text-white py-3 transition-all duration-300`}>
                             <span>Orders</span>
                             <div className="rounded-md w-7 aspect-square bg-gray-700 text-gray-200 text-sm flex justify-center items-center">{data.ordersCount}</div>
                         </li>
                     </Link>
-                    <Link onClick={() => setIsRedirecting(true)} href='/profile/addresses'>
+                    <Link ref={addressesRef} onClick={() => setIsRedirecting(true)} href='/profile/addresses'>
                         <li onMouseEnter={() => handleTabHover(3)} onMouseLeave={placeBorder} className={`whitespace-nowrap min-w-fit flex items-center gap-x-2 text-center text-2xl text-white py-3 transition-all duration-300 `}>
                             <span>Addresses</span>
                             <div className="rounded-md w-7 aspect-square bg-gray-700 text-gray-200 text-sm flex justify-center items-center">{data.addressesCount}</div>
                         </li>
                     </Link>
-                    <Link onClick={() => setIsRedirecting(true)} href='/profile/payments'>
+                    <Link ref={paymentsRef} onClick={() => setIsRedirecting(true)} href='/profile/payments'>
                         <li onMouseEnter={() => handleTabHover(4)} onMouseLeave={placeBorder} className={`whitespace-nowrap min-w-fit flex items-center gap-x-2 text-center text-2xl text-white py-3 transition-all duration-300 `}>
                             <span>Payments</span>
                             <div className="rounded-md w-7 aspect-square bg-gray-700 text-gray-200 text-sm flex justify-center items-center">{data.paymentsCount}</div>
